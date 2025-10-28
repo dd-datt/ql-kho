7c26d58c5fdd33d468afbfafff0f92f28faf18ad
Nguyen Van Dat <vdat319@gmail.com>
2025-10-28 17:20:18 +0700

    reset fe

--- ql_kho_fe/src/app/app.ts
+++ ql_kho_fe/src/app/app.ts
@@ -1,21 +1,11 @@
-import {Component, signal} from '@angular/core';
-import {Home} from './home/home';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [Home],
-  template: `
-    <main>
-      <header class="brand-name">
-        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-      </header>
-      <section class="content">
-        <app-home></app-home>
-      </section>
-    </main>
-  `,
-  styleUrl: './app.css'
+  imports: [],
+  template: ` <main></main> `,
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('ql_kho_fe');
+  // protected readonly title = signal('ql_kho_fe');
 }
